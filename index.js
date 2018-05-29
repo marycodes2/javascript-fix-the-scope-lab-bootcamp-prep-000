9f62e1f5854b503acec193b58d31d82998d8d651
mary.e.beliveau@gmail.com Beliveau <mary.e.beliveau@gmail.com>
2018-05-29 20:37:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 myAnimal()
 var theFunk = funkyFunction()
-hello = theFunk.function()
+hello = funkyFunction.function()
 console.log(hello)
